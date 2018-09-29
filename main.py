bc9016f230cad767249df0c9fdc524e8c148dfa6
Kyohei Oka <0622okakyo@gmail.com>
2018-09-29 16:08:23 +0900

    提出

--- main.py
+++ main.py
@@ -30,6 +30,22 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 
+def line_answer(place):
+    data={'京都市':3404,'四条':3414,'河原町':3402}
+    apikey='0600c456734c0f1315878fc5aeb29fa2&'
+    place=data[place]
+    url='http://api.gnavi.co.jp/RestSearchAPI/20150630/?keyid={key}&format=json&category_s=RSFST09004&areacode_s=AREAS{place}'.format(key=apikey,place=place)
+
+    html=requests.get(url)
+
+    data=json.loads(html.text)
+
+    with open('restcsvaurant.csv','w',newline='',encoding='utf-8') as w:
+        writer=csv.writer(w)
+        for k in data['rest']:
+            writer.writerow([k['name'],k['url']])
+
+
 app = Flask(__name__)
 
 # get channel_secret and channel_access_token from your environment variable
