edd4923a7d78649c729ea69c5a63125286a965c2
silloi <silloi1993@gmail.com>
2018-10-02 16:29:03 +0900

    if not None

--- main.py
+++ main.py
@@ -113,14 +113,14 @@ def message_text(event):
     if '飲' in event.message.text:
         text_split = re.split('[でへ]', event.message.text)
         place = text_split[0]
-        line_get_data(place)
-        nomiyas = line_answer()
-        nomiya = random.choice(nomiyas)
-        TEXT="{}へ飲みに行くぞ！ {}".format(nomiya[0], nomiya[1])
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=TEXT) #event.message.text がメッセージの本文
-        )
+        if line_get_data(place) is not None:
+            nomiyas = line_answer()
+            nomiya = random.choice(nomiyas)
+            TEXT="{}へ飲みに行くぞ！ {}".format(nomiya[0], nomiya[1])
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=TEXT) #event.message.text がメッセージの本文
+            )
 
 
 if __name__ == "__main__":
