a1cfe8bec3a2331664721383e9fac68111067fd0
silloi <silloi1993@gmail.com>
2018-10-02 09:55:43 +0900

    'areacode_s reverted

--- main.py
+++ main.py
@@ -34,12 +34,12 @@ from linebot.models import (
 areacode_s = ''
 #スクレイピングでお店のデータをcsvデータで取得する。
 def line_get_data(place):
-    f = open('area_s.json', 'r')
-    area = json.load(f)
-    for x in area_s:
-        areaname_s = area_s[x]['areaname_s']
-        if place in areaname_s:
-            areacode_s = area_s['areacode_s']
+#    f = open('area_s.json', 'r')
+#    area = json.load(f)
+#    for x in area_s:
+#        areaname_s = area_s[x]['areaname_s']
+#        if place in areaname_s:
+#            areacode_s = area_s['areacode_s']
     data={'京都市':3404,'四条':3414,'河原町':3402}
     apikey='0600c456734c0f1315878fc5aeb29fa2&'
     place=data[place]
@@ -112,7 +112,7 @@ def message_text(event):
     line_get_data(place)
     nomiyas = line_answer()
     nomiya = random.choice(nomiyas)
-    TEXT="{}へ飲みに行くぞ！ {} {}".format(nomiya[0], nomiya[1], areacode_s)
+    TEXT="{}へ飲みに行くぞ！ {}".format(nomiya[0], nomiya[1]) #, areacode_s)
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=TEXT) #event.message.text がメッセージの本文
