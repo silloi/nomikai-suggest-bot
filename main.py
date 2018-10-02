d3569923a5872f69af6ec01610154020b46e43ed
silloi <silloi1993@gmail.com>
2018-10-02 11:35:46 +0900

    if condition modified

--- main.py
+++ main.py
@@ -103,11 +103,15 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def message_text(event):
-    if 'へ飲み' or 'で飲' not in event.message.text:
+    if 'へ飲み' in event.message.text:
+        text_split = event.message.text.split("へ")
+    elif 'で飲' in event.message.text:
+        text_split = event.message.text.split("で")
+    else:
         return None
     # この辺に地名をAPIに投げるコードを記述
     # 得られた店名とURLを nomiya_info に格納
-    text_split = event.message.text.split("へ")
+    # text_split = event.message.text.split("へ")
     place = text_split[0]
     line_get_data(place)
     nomiyas = line_answer()
